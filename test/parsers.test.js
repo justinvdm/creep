d89c525fade095df23345be07bfa43b666631ed6
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-11 21:22:46 +0200

    Move matter parser tests into its own test module

--- test/parsers.test.js
+++ test/parsers.test.js
@@ -258,35 +258,4 @@ describe("creep.parsers", function() {
       });
     });
   });
-
-  describe("bundled creep.parsers", function() {
-    describe("matter", function() {
-      var matter;
-      
-      beforeEach(function() {
-        matter = creep.parsers.get('matter');
-      });
-
-      it("should parse and return a file's front matter metadata", function(done) {
-        matter(paths.fixtures('code/js/fib/memoize.js'))
-          .then(function(metadata) {
-            assert.deepEqual(metadata, {
-              name: 'fibonnaci sequence using memoization',
-              tags: ['memoize']
-            });
-          })
-          .then(done, done);
-      });
-
-      describe("if no matter parser is found for the requested format", function() {
-        it("should errback", function(done) {
-          matter('does/not/matter.js', {matterFormat: 'bleh'})
-            .catch(function(e) {
-              assert.equal(e.message, "No parser found for format 'bleh'");
-            })
-            .then(done, done);
-        });
-      });
-    });
-  });
 });
