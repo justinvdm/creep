7b6f1d8ba03d1e83ded3a1d0f1ee3693d70c1d52
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-13 08:39:16 +0200

    Remove old api method stubs that won't be happening anymore

--- src/index.js
+++ src/index.js
@@ -7,24 +7,13 @@ require('./filters/js');
 require('./filters/coffee');
 require('./parsers/matter');
 
+
 creep.config = require('./config');
 creep.crawl = require('./crawl');
 creep.query = require('./query');
 creep.filters = require('./filters');
 creep.parsers = require('./parsers');
 creep.utils = require('./utils');
-
-
-creep.list = function(query) {
-};
-
-
-creep.link = function(query) {
-};
-
-
-creep.unlink = function(query) {
-};
 
 
 _.each(config.parsers, function(parser, name) {
