883e578b3203b4334297ef7e256e87f68f9a7523
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-13 15:49:32 +0200

    Fix false positive js filter tests

--- test/filters.js.test.js
+++ test/filters.js.test.js
@@ -2,23 +2,23 @@ describe("creep.filters:js", function() {
   var filter = creep.filters.get('js');
 
   it("should run the given query", function() {
-    return filter("foo.bar.baz === 'qux'")('a.md', {foo: {bar: {baz: 'qux'}}})
-      .then(function(match) {
-        assert.strictEqual(match, true);
-      });
+    var f = filter("foo.bar.baz === 'qux'");
+    return f('a.md', {foo: {bar: {baz: 'qux'}}}).then(function(match) {
+      assert.strictEqual(match, true);
+    });
   });
 
   it("should return false if `TypeError`s are encountered", function() {
-    filter("foo.bar.baz")('a.md', {foo: {}})
-      .then(function(match) {
-        assert.strictEqual(match, false);
-      });
+    var f = filter("foo.bar.baz");
+    return f('a.md', {foo: {}}).then(function(match) {
+      assert.strictEqual(match, false);
+    });
   });
 
   it("should return false if `ReferenceError`s are encountered", function() {
-    filter("foo")('a.md', {})
-      .then(function(match) {
-        assert.strictEqual(match, false);
-      });
+    var f = filter("foo");
+    return f('a.md', {}) .then(function(match) {
+      assert.strictEqual(match, false);
+    });
   });
 });
